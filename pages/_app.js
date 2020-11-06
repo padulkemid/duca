1c1ca44bf2dc916e684df03aa6e0a6a2d691e929
padulkemid <bringitdown0@gmail.com>
2020-11-06 18:55:39 +0700

    server side jss removal to attach mui on next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+// react
+import { useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// next
+import Head from 'next/head';
+
+// @material-ui core
+import { ThemeProvider } from '@material-ui/core/styles';
+
+// prop-types
+import PropTypes from 'prop-types';
+
+// locals
+import { lightMode, darkMode } from '../styles/themes';
+import '../styles/globals.scss';
+
+export default function App({ Component, pageProps }) {
+  const [mode, setMode] = useState(true);
+  const selectedMode = mode ? lightMode : darkMode;
+
+  useEffect(() => {
+    // remove the server-side injected CSS. ( from material-ui )
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles) jssStyles.parentElement.removeChild(jssStyles);
+
+    // check local storage for theme mode
+    const getMode = localStorage.getItem('mode');
+    if (getMode === 'light') {
+      setMode(true);
+    } else {
+      setMode(false);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={selectedMode}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};

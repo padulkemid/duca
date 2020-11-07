b00efe120371472bdfa1d9347e3a3222f5f9ac95
padulkemid <bringitdown0@gmail.com>
2020-11-08 03:50:24 +0700

    Change font style on condition and date.

--- components/weather/location_info.js
+++ components/weather/location_info.js
@@ -1,21 +1,34 @@
 // @material-ui core
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 // local helpers
 import { Emoji } from '../../utils/general_helper';
 
+const useStyles = makeStyles({
+  date: {
+    fontSize: '0.9rem',
+  },
+  condition: {
+    fontSize: '1.8rem',
+  },
+});
+
 export default function LocationInfo() {
+  const classes = useStyles();
   return (
     <Grid container spacing={2} direction="column" alignItems="flex-start">
       <Grid item xs>
         <Typography variant="h2">Bogor</Typography>
       </Grid>
       <Grid item xs>
-        <Typography variant="body1">Saturday, 7 November</Typography>
+        <Typography variant="overline" className={classes.date}>
+          Saturday, 7 November
+        </Typography>
       </Grid>
       <Grid item xs>
-        <Typography variant="h4">
+        <Typography variant="h5" className={classes.condition}>
           Raining
           <Emoji symbol="🌧️" label="Raining" />
         </Typography>

b546938ee2346e161f47e371d12a28022695d495
padulkemid <bringitdown0@gmail.com>
2020-11-07 16:14:37 +0700

    Pass changeMode func to ModeChange component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
-// @material-ui core
-import Button from '@material-ui/core/Button';
+// prop-types
+import PropTypes from 'prop-types';
 
 // local components
 import SEO from '../components/seo';
 import Layout from '../components/layout';
 import Header from '../components/header';
+import ModeChange from '../components/mode_change';
 
 // TODO: change this
 export default function Home({ changeMode }) {
   return (
     <Layout>
+      <ModeChange changeMode={changeMode} />
       <SEO />
       <Header />
-      <Button variant="contained" onClick={changeMode}>
-        change mode
-      </Button>
     </Layout>
   );
 }
+
+Home.propTypes = {
+  changeMode: PropTypes.func.isRequired,
+};

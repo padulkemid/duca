eb3ec69e3012d0399e163f7ab67f2b06674dee5d
padulkemid <bringitdown0@gmail.com>
2020-11-08 04:28:28 +0700

    Added placeholder function for nearby circle areas.

--- components/bottom.js
+++ components/bottom.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 // local components
 import TitleBar from './title_bar';
 import Forecasts from './weather/forecasts';
+import NearbyCircleArea from './weather/nearby_circle_area';
 
 const useStyles = makeStyles((theme) => ({
   grid: {
@@ -51,9 +52,50 @@ function getForecastsInfo() {
   return data;
 }
 
+function getNearbyCircleAreas() {
+  const data = [
+    {
+      id: 1,
+      imgSrc: '/weather/storm.png',
+      imgAlt: 'Thunderstorm',
+      condition: 'Thunderstorm',
+      city: 'Jakarta',
+      temp: '26.92°C',
+    },
+    {
+      id: 2,
+      imgSrc: '/weather/cloudy-1.png',
+      imgAlt: 'Clouds',
+      condition: 'Broken Clouds',
+      city: 'Tangerang',
+      temp: '32.02°C',
+    },
+    {
+      id: 3,
+      imgSrc: '/weather/rain-2.png',
+      imgAlt: 'Rain',
+      condition: 'Light Rain',
+      city: 'Depok',
+      temp: '33.83°C',
+    },
+    {
+      id: 4,
+      imgSrc: '/weather/sun.png',
+      imgAlt: 'Sunny',
+      condition: 'Clear Sky',
+      city: 'Sukabumi',
+      temp: '34.02°C',
+    },
+  ];
+
+  return data;
+}
+
 export default function Bottom() {
   const classes = useStyles();
   const forecasts = getForecastsInfo();
+  const nearbyCircleAreas = getNearbyCircleAreas();
+
   return (
     <>
       <TitleBar text="Forecasts" />
@@ -65,6 +107,13 @@ export default function Bottom() {
         ))}
       </Grid>
       <TitleBar text="Nearby Circle Area" />
+      <Grid container spacing={2} className={classes.grid}>
+        {nearbyCircleAreas.map(({ id, ...info }) => (
+          <Grid item key={id} xs={12} md={6} lg>
+            <NearbyCircleArea {...info} />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }

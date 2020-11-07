a498b9074bb9c180bbd7b4e56cffa14bfc97edf1
padulkemid <bringitdown0@gmail.com>
2020-11-07 07:04:40 +0700

    added override config to light and dark themes

--- styles/themes/light.js
+++ styles/themes/light.js
@@ -1,9 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import fonts from './fonts';
+import overrideConfig from './overrides';
 
 const theme = createMuiTheme({
   typography: {
     ...fonts,
+  },
+  overrides: {
+    ...overrideConfig,
   },
   palette: {
     type: 'light',
